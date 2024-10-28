88ef02b2b25a18e2eb6b3d4b24dd20f77ca39532
Pulkit Khandelwal <kpulkit95@gmail.com>
2024-10-28 14:11:57 -0400

    Update screenshots_fs_viz.sh

--- visualization/screenshots_fs_viz.sh
+++ visualization/screenshots_fs_viz.sh
@@ -12,3 +12,8 @@ freeview -f ${SUBJECTS_DIR}/${subj}/surf/rh.pial:annot=${SUBJECTS_DIR}/${subj}/l
 --viewport '3d' --viewsize 900 900 -cam zoom 1.0 -ss ${SUBJECTS_DIR}/screenshots/${subj}.png
 
 done;
+
+:<<'VIZ'
+freeview -v mri/orig.mgz mri/aparc.DKTatlas+aseg.mgz:colormap=LUT \
+-f surf/rh.pial:annot=label/rh.aparc.DKTatlas.annot:edgecolor=yellow surf/rh.inflated:annot=label/rh.aparc.DKTatlas.annot:edgecolor=yellow
+VIZ
