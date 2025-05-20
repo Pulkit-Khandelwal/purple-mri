6be7024e2045c09c126d641034254740813b7745
Pulkit Khandelwal <kpulkit95@gmail.com>
2025-05-20 18:19:18 -0400

    Update prepare_segm_files.sh

--- purple_mri/prepare_segm_files.sh
+++ purple_mri/prepare_segm_files.sh
@@ -7,10 +7,25 @@ for i in $str_split; do subjects+=($i) ; done
 for subj in "${subjects[@]}"
 do
     echo ${subj}
+    cp ${segm_path}/${subj}_reslice.nii.gz ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected_orig.nii.gz
+    c3d ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected_orig.nii.gz -replace 6 7 -o ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected_orig.nii.gz
 
-    cp ${segm_path}/${subj}.nii.gz ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz
+    ##### Let's get the CC for each label and then merge it
+    base_file=${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected_orig.nii.gz
+    numlabel_array=(1 2 3 4 5 7 8 9 10)
+    for numlabel in "${numlabel_array[@]}"
+    do
+    echo "CC for label " ${numlabel}
+        c3d ${base_file} -retain-labels ${numlabel} -replace ${numlabel} 1 -comp -threshold 1 1 1 0 -replace 1 ${numlabel} -o ${generated_files_folder}/${subj}_label_${numlabel}_cc.nii.gz
+    done
 
-    c3d ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz -replace 6 7 -o ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz
+    c3d ${generated_files_folder}/${subj}_label_1_cc.nii.gz ${generated_files_folder}/${subj}_label_2_cc.nii.gz ${generated_files_folder}/${subj}_label_3_cc.nii.gz \
+    ${generated_files_folder}/${subj}_label_4_cc.nii.gz ${generated_files_folder}/${subj}_label_5_cc.nii.gz ${generated_files_folder}/${subj}_label_7_cc.nii.gz \
+    ${generated_files_folder}/${subj}_label_8_cc.nii.gz ${generated_files_folder}/${subj}_label_9_cc.nii.gz ${generated_files_folder}/${subj}_label_10_cc.nii.gz \
+    -add -add -add -add -add -add -add -add -o ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz
+
+    echo "CC obtained for each label and merged to get a nicer segmentation with no erroneous segmentation labels hanging around"
+    c3d ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz -dup -lstat
 
     # REPLACED: BEFORE 9 0; NOW 9 192 (freeSurfer label for CC)
     # REPLACED: Correct labels for subcortical regions: 2 51 3 50 these are correct.
