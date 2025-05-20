addbf7d7fa1c508ad546ed88e4d5aacf4d1f95a4
Pulkit Khandelwal <kpulkit95@gmail.com>
2025-05-20 14:05:49 -0400

    Update flip_conform.sh

--- misc_scripts/flip_conform.sh
+++ misc_scripts/flip_conform.sh
@@ -1,16 +1,16 @@
 # Check the hemisphere
-  hemis_check=${subj: -1}
-  echo $hemis_check
-  var=R
-  if [ "$hemis_check" == "$var" ]; then
-          echo "Right hemis"
-      else
-          echo "Left hemis"
-      # flip if left and over-write the mri and segm files
-      c3d ${src_mri}/${subj}.nii.gz -flip y -o ${src_mri}/${subj}.nii.gz
-      c3d ${src_segm}/${subj}.nii.gz -flip y -o ${src_segm}/${subj}.nii.gz
-  fi
-    
+hemis_check=${subj: -1}
+echo $hemis_check
+var=R
+if [ "$hemis_check" == "$var" ]; then
+        echo "Right hemis"
+    else
+        echo "Left hemis"
+    # flip if left and over-write the mri and segm files
+    c3d ${src_mri}/${subj}.nii.gz -flip y -o ${src_mri}/${subj}.nii.gz
+    c3d ${src_segm}/${subj}.nii.gz -flip y -o ${src_segm}/${subj}.nii.gz
+fi
+  
 # conform the files to MNI space
 mri_convert ${src_mri}/${subj}.nii.gz ${dst_for_mni_mri}/${subj}_mri_conform.mgz --conform
 mri_convert ${src_segm}/${subj}.nii.gz ${dst_for_mni_segm}/${subj}_segm_conform.mgz --conform
