d0156692c69ff95f65d8182572d6167d3e46fdff
Pulkit Khandelwal <kpulkit95@gmail.com>
2025-09-19 21:46:11 -0400

    Update prepare_segm_files.sh

--- purple_mri/prepare_segm_files.sh
+++ purple_mri/prepare_segm_files.sh
@@ -2,6 +2,7 @@ working_dir=$1
 str_split=$2
 generated_files_folder=$3
 segm_path=$4
+hemis=$5
 for i in $str_split; do subjects+=($i) ; done
 
 for subj in "${subjects[@]}"
@@ -48,8 +49,8 @@ do
     cp ${generated_files_folder}/${subj}_wm.mgz ${generated_files_folder}/${subj}_filled.mgz
 
     c3d ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz -replace 1 42 2 41 3 41 4 41 5 41 7 41 8 41 9 41 10 0 -o ${generated_files_folder}/${subj}_ribbon.nii.gz
-    c3d ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz -retain-labels 1 -o ${generated_files_folder}/${subj}_rh.ribbon.nii.gz
+    c3d ${generated_files_folder}/${subj}_reslice_aseg_ready_with_overlap_corrected.nii.gz -retain-labels 1 -o ${generated_files_folder}/${subj}_${hemis}.ribbon.nii.gz
 
-    mri_convert ${generated_files_folder}/${subj}_rh.ribbon.nii.gz ${generated_files_folder}/${subj}_rh.ribbon.mgz
+    mri_convert ${generated_files_folder}/${subj}_${hemis}.ribbon.nii.gz ${generated_files_folder}/${subj}_${hemis}.ribbon.mgz
     mri_convert ${generated_files_folder}/${subj}_ribbon.nii.gz ${generated_files_folder}/${subj}.ribbon.mgz
 done;
