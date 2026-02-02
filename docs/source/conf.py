3f97a0b72dee3c9b8c2fbc27831303a26416e3fe
Pulkit Khandelwal <kpulkit95@gmail.com>
2026-02-01 20:37:58 -0500

    Update project information in Sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'purple-mri'
+copyright = 'Pulkit Khandelwal'
+author = 'Pulkit Khandelwal'
 
 release = '0.1'
 version = '0.1.0'
