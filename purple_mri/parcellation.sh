ad1ace001ab201d369e5fff3a376b3880daa865e
tsantini <26204473+tsantini@users.noreply.github.com>
2026-05-06 14:52:59 -0400

    Correcting typo preventing cortical parcellation

--- purple_mri/parcellation.sh
+++ purple_mri/parcellation.sh
@@ -181,7 +181,7 @@ pctsurfcon --s ${subj} --${hemis}-only
 mri_brainvol_stats --subject ${subj}
 
 atlases=(DKTatlas aparc a2009s brainnetome HCP-MMP1.glasser Schaefer2018_400Parcels_17Networks julich economo)
-for subj in "${atlas[@]}"
+for atlas in "${atlases[@]}"
 do
 echo ${atlas}
 
@@ -202,7 +202,7 @@ done
 ######## WMParc
 cd ${SUBJECTS_DIR}/${subj}/mri
 mri_surf2volseg --o wmparc.mgz \
---label-wm --i aparc+aseg.mgz --threads ${num_threads} \
+--label-wm --i aparc.mgz --threads ${num_threads} \
 --lh-annot ${SUBJECTS_DIR}/${subj}/label/lh.aparc.annot 3000 \
 --lh-cortex-mask ${SUBJECTS_DIR}/${subj}/label/lh.cortex.label \
 --lh-white ${SUBJECTS_DIR}/${subj}/surf/lh.white \
